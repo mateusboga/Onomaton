37304a7a8306b59354f5b0f4e3dca8d9c4316d54
Mateus <mateusboga@gmail.com>
2015-03-25 23:39:48 +0000

    greek names

--- sources/synth.js
+++ sources/synth.js
@@ -33,6 +33,30 @@ function createName(n) {
 	}; click.play(); return Text;
 }
 
+function RandGreek() {
+	Name = createGreekName(3);
+	R();
+}
+
+function createGreekName(n) {
+	var click = new Audio('sources/click.wav');
+	var Text = '';
+	var StartSyll = [
+		'arch','al','pal','ga','be','ae','on','aph','eph','xi','ach','ech','ar','he','ze','ch','er'
+	];
+	var MidSyll = [
+		'la','ai','arch','ae','ar','i','ze','o','e','a','ro','m','ik','ph','de','psi','ron','on','r','s'
+	];
+	var EndSyll = [
+		'on','os','lon','a','e','es','o','i','ae','ea'
+	];
+	for(i=0; i < n; i++) {
+		if(i == 0){ Text += StartSyll[( Math.floor( Math.random() * StartSyll.length ))]; }
+		if(i > (n-2)){ Text += EndSyll[( Math.floor( Math.random() * EndSyll.length ))]; }
+		else { Text += MidSyll[( Math.floor( Math.random() * MidSyll.length ))]; }
+	}; click.play(); return Text;
+}
+
 // Others
 
 function isOdd(num) { return num % 2;}
