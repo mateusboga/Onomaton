e891959b4e69f27c37448a0aec048a76991e0609
Mateus <mateusboga@gmail.com>
2015-03-25 22:51:27 +0000

    update3

--- sources/synth.js
+++ sources/synth.js
@@ -18,3 +18,21 @@ function createString(n) {
 		Text += Chars.charAt( Math.floor( Math.random() * Chars.length));
 	}; click.play(); return Text;
 }
+
+function RandSyll() {
+	Name = createName(8);
+	R();
+}
+
+function createName(n) {
+	var click = new Audio('sources/click.wav');
+	var Text = '', Vowels = 'aeiouy', Cons = 'wybcdfghjklmnpqrstvxzaeiou';
+	for(i=0; i < n; i++) {
+		if(isOdd(i)){Text += Cons.charAt( Math.floor( Math.random() * Cons.length ));}
+		else{ Text += Vowels.charAt( Math.floor( Math.random() * Vowels.length ));}
+	}; click.play(); return Text;
+}
+
+// Others
+
+function isOdd(num) { return num % 2;}
