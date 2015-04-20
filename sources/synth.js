3d28c7b4fd2d730fe437fb5dc495dfe9e0b6bd35
Mateus <mateusboga@gmail.com>
2015-04-20 01:17:10 +0100

    update 3

--- sources/synth.js
+++ sources/synth.js
@@ -6,10 +6,15 @@ function R() {
 	input1.value = Name;
 	console.log(Name);
 }
+function L() {
+	sylln = document.getElementById('SyllC').value;
+}
 
-var stringn = 8, namen = 8, greekn = 3, norsen = 2;
+//var stringn = 8, namen = 8, greekn = 3, norsen = 2;
+var sylln = 3;
 function RandString() {
-	Name = createString(stringn);
+	L();
+	Name = createString(sylln*2);
 	R();
 }
 
@@ -22,7 +27,8 @@ function createString(n) {
 }
 
 function RandSyll() {
-	Name = createName(namen);
+	L();
+	Name = createName(sylln*2);
 	R();
 }
 
@@ -32,11 +38,49 @@ function createName(n) {
 	for(i=0; i < n; i++) {
 		if(isOdd(i)){Text += Cons.charAt( Math.floor( Math.random() * Cons.length ));}
 		else{ Text += Vowels.charAt( Math.floor( Math.random() * Vowels.length ));}
-	}; click.play(); return Text;
+	}; 
+	Text = PronFilter(Text);
+	click.play(); return Text;
+}
+
+function PronFilter(oldtext){
+	var newtext = '';
+	newtext = oldtext;
+	newtext = newtext.replace(/q$/, "c")
+	newtext = newtext.replace('qo','quo');
+	newtext = newtext.replace('qe','que');
+	newtext = newtext.replace('qa','qua');
+	newtext = newtext.replace('qi','qui');
+	newtext = newtext.replace('yj','ij');
+	newtext = newtext.replace('yy','y');
+	newtext = newtext.replace('uu','u');
+	newtext = newtext.replace('ee','e');
+	newtext = newtext.replace('ii','i');
+	newtext = newtext.replace('aa','a');
+	newtext = newtext.replace('iw','yow');
+	newtext = newtext.replace('iey','ihy');
+	newtext = newtext.replace('yk','ic');
+	newtext = newtext.replace('ys','is');
+	newtext = newtext.replace('oej','ojey');
+	newtext = newtext.replace('oei','wey');
+	newtext = newtext.replace('uw','ow');
+	newtext = newtext.replace('ieuo','ihewo');
+	newtext = newtext.replace('iaue','ihawe');
+	newtext = newtext.replace('aoyi','auyi');
+	newtext = newtext.replace('owu','ohu');
+	newtext = newtext.replace('ioae','iohay');
+	newtext = newtext.replace('jo','yo');
+	newtext = newtext.replace('uiye','uhiye');
+	newtext = newtext.replace('yij','yih');
+	newtext = newtext.replace('quq','quoc');
+	newtext = newtext.replace('ty','try');
+	
+	return newtext;
 }
 
 function RandGreek() {
-	Name = createGreekName(greekn);
+	L();
+	Name = createGreekName(sylln);
 	R();
 }
 
@@ -61,6 +105,7 @@ function createGreekName(n) {
 		else { Text += MidSyll[( Math.floor( Math.random() * MidSyll.length ))]; }
 	}; 
 	Text = GreekFilter(Text);
+	//Text = PtFilter(Text);
 	click.play(); return Text;
 }
 function GreekFilter(oldtext) {
@@ -87,7 +132,8 @@ function GreekFilter(oldtext) {
 //setInterval(function(){ Name = createGreekName(3);R(); },1000)
 
 function RandNord() {
-	Name = createNorseName(norsen);
+	L();
+	Name = createNorseName(sylln);
 	R();
 }
 
@@ -113,6 +159,73 @@ function createNorseName(n) {
 }
 function NorseFilter(old){
 	return old;
+}
+
+function PtFilter(oldtext){
+	var newtext = '';
+	newtext = oldtext;
+	newtext = newtext.replace('ch','c');
+	newtext = newtext.replace('us','o');
+	newtext = newtext.replace('os','o');
+	newtext = newtext.replace('ph','f');
+	newtext = newtext.replace('is','isa');
+	newtext = newtext.replace('k','qu');
+	newtext = newtext.replace('y','i');
+	newtext = newtext.replace('on','\u00E3'+'o');
+	newtext = newtext.replace('hi','i');
+	newtext = newtext.replace('th','t');
+	return newtext;
+}
+
+function RandAtla() {
+	L();
+	Name = createAtladonName(sylln);
+	R();
+}
+
+function createAtladonName(n) {
+	var click = new Audio('sources/click.wav');
+	var Text = '';
+	var StartSyll = [
+		'ark','akh','bak','veth','ghim','el','del','ea','ef','ia','mah','thra'
+	];
+	var MidSyll = [
+		'ir','ed','op','el','er','ksh','osh','iek','otr','al','rn'
+	];
+	var EndSyll = [
+		'eln','uln','oln','on','don','chron','ern','elro','kno','os','nos','al','iln','kreon'
+	];
+	for(i=1; i < n; i++) {
+		if(i == 1){ Text += StartSyll[( Math.floor( Math.random() * StartSyll.length ))]; }
+		if(i == (n-1)){ Text += EndSyll[( Math.floor( Math.random() * EndSyll.length ))]; }
+		else { Text += MidSyll[( Math.floor( Math.random() * MidSyll.length ))]; }
+	}; 
+	Text = AtlaFilter(Text);
+	click.play(); return Text;
+}
+
+function AtlaFilter(oldtext){
+	var newtext = '';
+	newtext = oldtext;
+	newtext = newtext.replace('ii','i');
+	newtext = newtext.replace('pp','p');
+	newtext = newtext.replace('ee','e');
+	newtext = newtext.replace('nn','n');
+	newtext = newtext.replace('bb','b');
+	newtext = newtext.replace('ss','s');
+	newtext = newtext.replace('tt','t');
+	newtext = newtext.replace('rr','r');
+	newtext = newtext.replace('ll','l');
+	newtext = newtext.replace('oo','o');
+	newtext = newtext.replace('thth','th');
+	newtext = newtext.replace('dd','d');
+	newtext = newtext.replace('aa','a');
+	newtext = newtext.replace('mm','m');
+	newtext = newtext.replace('tht','th');
+	newtext = newtext.replace('khk','k');
+	newtext = newtext.replace('yy','y');
+	newtext = newtext.replace('kk','k');
+	return newtext;
 }
 
 // Others
